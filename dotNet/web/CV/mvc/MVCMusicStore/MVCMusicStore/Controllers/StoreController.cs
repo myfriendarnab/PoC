3654d7005cea70afc31f2e9f66362b5c5d81d7ee
arnab bhattacharya <arnab bhattacharya>
2016-12-22 21:12:03 +0530

    Initaiting PoC

---
+++ dotNet/web/CV/mvc/MVCMusicStore/MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,43 @@
+﻿using MVCMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // GET: Store
+        public string Browse(string genre)
+        {
+            var msg = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return msg;
+        }
+
+        // GET: Store
+        public string Details(int id)
+        {
+            var msg = "Store.Details, ID = " + id;
+            return msg;
+        }
+
+        // GET: Store
+        public ActionResult Albums(int id)
+        {
+            var albums = new List<Album>();
+            for (int i = 0; i < id; i++)
+            {
+                albums.Add(new Album { Title = "Album" + i });
+            }
+            return View(albums);
+        }
+    }
+}
---
+++ dotNet/web/CV/mvc/MVCMusicStore/MVCMusicStore/Samples/ShoppingCartViewModel/ProductModel.cs
@@ -0,0 +1,8 @@
+namespace MVCMusicStore.Models
+{
+    public class Product
+	{
+        public string Title { get; set; }
+		public decimal Price { get; set; }
+    }
+}
